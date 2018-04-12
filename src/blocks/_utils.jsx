1337ed9ceee863b2dc00893a199b64e30ce62dae
Nikita <nkdevinfo@gmail.com>
2018-04-12 15:47:22 +0300

    slightly fixed eslint errors

--- src/blocks/_utils.jsx
+++ src/blocks/_utils.jsx
@@ -78,13 +78,17 @@ export const renderStyles = () => {
 };
 
 /**
+ *
  * camelCaseToDash('userId') => "user-id"
  * camelCaseToDash('waitAMoment') => "wait-a-moment"
  * camelCaseToDash('TurboPascal') => "turbo-pascal"
  *
  * https://gist.github.com/youssman/745578062609e8acac9f
+ *
+ * @param {string} str - camel-cased string.
+ * @return {string} - new dashed string.
  */
-function camelCaseToDash (str) {
+function camelCaseToDash( str ) {
     if ( typeof str !== 'string' ) {
         return str;
     }
