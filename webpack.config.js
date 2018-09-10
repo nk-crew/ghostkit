c3815b9b35b01e5978adf5c5571a1e39721d515f
Nikita <nkdevinfo@gmail.com>
2018-09-10 21:11:05 +0300

    improved svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const md5 = require( 'md5' );
 
 module.exports = {
     module: {
@@ -19,9 +20,18 @@ module.exports = {
                 ],
             }, {
                 test: /\.svg$/,
-                use: [
-                    '@svgr/webpack',
-                ],
+                use: ( { resource } ) => ( {
+                    loader: '@svgr/webpack',
+                    options: {
+                        svgoConfig: {
+                            plugins: [ {
+                                cleanupIDs: {
+                                    prefix: `ghostkit-${ md5( resource ) }-`,
+                                },
+                            } ],
+                        },
+                    },
+                } ),
             },
         ],
     },
