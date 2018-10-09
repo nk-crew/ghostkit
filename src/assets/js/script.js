133f2aa4b2bea834f193086f8e4b3f58421822eb
Nikita <nkdevinfo@gmail.com>
2018-10-10 02:17:22 +0300

    improved Animate on Scroll extension (more options)

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -589,13 +589,48 @@ function prepareSR() {
 
         $this.addClass( 'data-ghostkit-sr-ready' );
 
-        const effect = $this.attr( 'data-ghostkit-sr' );
-        const data = {
-            distance: '50px',
-            origin: 'center',
+        // parse data from string.
+        // fade-right;duration:500;delay:1000
+        const data = $this.attr( 'data-ghostkit-sr' ).split( ';' );
+
+        let effect = data[ 0 ];
+        let distance = '50px';
+        let scale = 1;
+        let origin = effect.split( '-' );
+        if ( 2 === origin.length ) {
+            effect = origin[ 0 ];
+            origin = origin[ 1 ];
+
+            switch ( origin ) {
+            case 'up':
+                origin = 'bottom';
+                break;
+            case 'down':
+                origin = 'top';
+                break;
+            case 'right':
+                origin = 'right';
+                break;
+            case 'left':
+                origin = 'left';
+                break;
+            }
+        } else {
+            origin = 'center';
+            distance = 0;
+        }
+
+        if ( 'scale' === effect ) {
+            scale = 0.9;
+        }
+
+        const config = {
+            distance: distance,
+            origin: origin,
             opacity: 0,
-            scale: 1,
+            scale: scale,
             duration: 900,
+            delay: 0,
             reset: false,
             afterReveal() {
                 $this.removeAttr( 'data-ghostkit-sr' );
@@ -603,24 +638,21 @@ function prepareSR() {
             },
         };
 
-        switch ( effect ) {
-        case 'fade':
-            data.distance = 0;
-            break;
-        case 'fade-up':
-            data.origin = 'bottom';
-            break;
-        case 'zoom':
-            data.distance = 0;
-            data.scale = 0.9;
-            break;
-        case 'zoom-up':
-            data.origin = 'bottom';
-            data.scale = 0.9;
-            break;
+        // replace other data config.
+        if ( data.length > 1 ) {
+            data.forEach( ( item ) => {
+                const itemData = item.split( ':' );
+                if ( 2 === itemData.length ) {
+                    config[ itemData[ 0 ] ] = itemData[ 1 ];
+                }
+            } );
         }
 
-        reveal( this, data, 100 );
+        config.scale = parseFloat( config.scale );
+        config.duration = parseFloat( config.duration );
+        config.delay = parseFloat( config.delay );
+
+        reveal( this, config );
     } );
 }
 
