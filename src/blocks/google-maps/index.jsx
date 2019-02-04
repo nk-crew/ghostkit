425c71d10bd5ac4b5fd349102c2b359a6c6e7d98
Nikita <nkdevinfo@gmail.com>
2019-02-04 09:55:27 +0300

    remove className from Google Maps toolbar dropdown

--- src/blocks/google-maps/index.jsx
+++ src/blocks/google-maps/index.jsx
@@ -270,7 +270,6 @@ class GoogleMapsBlock extends Component {
                         ] }
                     >
                         <Dropdown
-                            className={ className }
                             renderToggle={ ( { onToggle } ) => (
                                 <IconButton
                                     label={ __( 'Style' ) }
