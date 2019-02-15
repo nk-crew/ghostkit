099fe19f01e220c9241fa9151f94528e79e94ea5
Nikita <nkdevinfo@gmail.com>
2019-02-15 12:35:19 +0300

    changed structure of badge format jsx file

--- src/blocks/_formats/badge/index.jsx
+++ src/blocks/_formats/badge/index.jsx
@@ -4,6 +4,8 @@ import './editor.scss';
 
 import PositionedAtSelection from './positioned-at-selection';
 
+const { __ } = wp.i18n;
+
 const {
     Component,
     Fragment,
@@ -18,86 +20,84 @@ const {
     ColorPalette,
 } = wp.editor;
 
-class BadgeFormat extends Component {
-    constructor() {
-        super( ...arguments );
-
-        this.toggleFormat = this.toggleFormat.bind( this );
-    }
-
-    toggleFormat( color, toggle = true ) {
-        const {
-            value,
-            onChange,
-        } = this.props;
-
-        const attributes = {
-            class: 'ghostkit-badge',
-        };
-
-        if ( color ) {
-            attributes.style = `background-color: ${ color };`;
-        }
-
-        const toggleFormat = toggle ? wp.richText.toggleFormat : wp.richText.applyFormat;
-
-        onChange( toggleFormat(
-            value,
-            {
-                type: 'ghostkit/badge',
-                attributes: attributes,
-            }
-        ) );
-    }
-
-    render() {
-        const {
-            value,
-            isActive,
-        } = this.props;
-
-        return (
-            <Fragment>
-                <RichTextToolbarButton
-                    icon="tag"
-                    title="Badge"
-                    onClick={ () => {
-                        this.toggleFormat();
-                    } }
-                    isActive={ isActive }
-                />
-                { isActive ? (
-                    <PositionedAtSelection
-                        key={ `${ value.start }${ value.end }` /* Used to force rerender on selection change */ }
-                    >
-                        <Popover
-                            focusOnMount={ false }
-                            position="bottom"
-                            className="ghostkit-format-badge-popover"
-                        >
-                            <ColorPalette
-                                value={ value }
-                                onChange={ ( color ) => {
-                                    this.toggleFormat( color, false );
-                                } }
-                            />
-                        </Popover>
-                    </PositionedAtSelection>
-                ) : '' }
-            </Fragment>
-        );
-    }
-}
-
 export const name = 'ghostkit/badge';
 
 export const settings = {
-    title: 'Badge',
+    title: __( 'Badge' ),
     tagName: 'span',
     className: null,
     attributes: {
         class: 'class',
         style: 'style',
     },
-    edit: BadgeFormat,
+    edit: class BadgeFormat extends Component {
+        constructor() {
+            super( ...arguments );
+
+            this.toggleFormat = this.toggleFormat.bind( this );
+        }
+
+        toggleFormat( color, toggle = true ) {
+            const {
+                value,
+                onChange,
+            } = this.props;
+
+            const attributes = {
+                class: 'ghostkit-badge',
+            };
+
+            if ( color ) {
+                attributes.style = `background-color: ${ color };`;
+            }
+
+            const toggleFormat = toggle ? wp.richText.toggleFormat : wp.richText.applyFormat;
+
+            onChange( toggleFormat(
+                value,
+                {
+                    type: name,
+                    attributes: attributes,
+                }
+            ) );
+        }
+
+        render() {
+            const {
+                value,
+                isActive,
+            } = this.props;
+
+            return (
+                <Fragment>
+                    <RichTextToolbarButton
+                        icon="tag"
+                        title="Badge"
+                        onClick={ () => {
+                            this.toggleFormat();
+                        } }
+                        isActive={ isActive }
+                    />
+                    { isActive ? (
+                        <PositionedAtSelection
+                            key={ `${ value.start }${ value.end }` /* Used to force rerender on selection change */ }
+                        >
+                            <Popover
+                                focusOnMount={ false }
+                                position="bottom"
+                                className="ghostkit-format-badge-popover"
+                            >
+                                <ColorPalette
+                                    value={ value }
+                                    onChange={ ( color ) => {
+                                        this.toggleFormat( color, false );
+                                    } }
+                                />
+                            </Popover>
+                        </PositionedAtSelection>
+                    ) : '' }
+                </Fragment>
+            );
+        }
+    },
 };
