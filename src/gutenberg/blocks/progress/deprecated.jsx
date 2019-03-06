3d8837ac91950a9289216afaa2c20b38ad46a80b
Nikita <nkdevinfo@gmail.com>
2019-03-06 19:09:00 +0300

    fixed progress bar xml import error (aria attributes bug)

--- src/gutenberg/blocks/progress/deprecated.jsx
+++ src/gutenberg/blocks/progress/deprecated.jsx
@@ -12,6 +12,154 @@ const {
 
 export default [
     {
+        // fixed importing demo content
+        // aria attributes not imported for some reason
+        // so we need to fix it manually
+        // https://wordpress.org/support/topic/bug-after-import-backward-compatibility/
+        ghostkit: {
+            customStylesCallback( attributes ) {
+                const styles = {
+                    '.ghostkit-progress-wrap': {
+                        height: attributes.height,
+                        borderRadius: attributes.borderRadius,
+                        backgroundColor: attributes.backgroundColor,
+                        '.ghostkit-progress-bar': {
+                            width: attributes.percent + '%',
+                            backgroundColor: attributes.color,
+                        },
+                    },
+                };
+
+                if ( attributes.hoverColor ) {
+                    styles[ '&:hover' ] = {
+                        '.ghostkit-progress-wrap': {
+                            '.ghostkit-progress-bar': {
+                                backgroundColor: attributes.hoverColor,
+                            },
+                        },
+                    };
+                }
+                if ( attributes.hoverBackgroundColor ) {
+                    styles[ '&:hover' ] = deepAssign( styles[ '&:hover' ] || {}, {
+                        '.ghostkit-progress-wrap': {
+                            backgroundColor: attributes.hoverBackgroundColor,
+                        },
+                    } );
+                }
+
+                return styles;
+            },
+            supports: {
+                styles: true,
+                spacings: true,
+                display: true,
+                scrollReveal: true,
+            },
+        },
+        supports: {
+            html: false,
+            className: false,
+            anchor: true,
+            align: [ 'wide', 'full' ],
+        },
+        attributes: {
+            caption: {
+                type: 'string',
+                source: 'html',
+                selector: '.ghostkit-progress-caption',
+                default: 'Progress Caption',
+            },
+            height: {
+                type: 'number',
+                default: 15,
+            },
+            percent: {
+                type: 'number',
+                default: 75,
+            },
+            borderRadius: {
+                type: 'number',
+                default: 2,
+            },
+            striped: {
+                type: 'boolean',
+                default: true,
+            },
+            animateInViewport: {
+                type: 'boolean',
+                default: false,
+            },
+            showCount: {
+                type: 'boolean',
+                default: false,
+            },
+            countPrefix: {
+                type: 'string',
+                default: '',
+            },
+            countSuffix: {
+                type: 'string',
+                default: '%',
+            },
+            color: {
+                type: 'string',
+                default: '#0366d6',
+            },
+            backgroundColor: {
+                type: 'string',
+                default: '#f3f4f5',
+            },
+            hoverColor: {
+                type: 'string',
+            },
+            hoverBackgroundColor: {
+                type: 'string',
+            },
+        },
+        save: function( props ) {
+            const {
+                caption,
+                height,
+                percent,
+                striped,
+                showCount,
+                countPrefix,
+                countSuffix,
+                animateInViewport,
+            } = props.attributes;
+
+            let className = 'ghostkit-progress';
+
+            className = applyFilters( 'ghostkit.blocks.className', className, {
+                ...{
+                    name: 'ghostkit/progress',
+                },
+                ...props,
+            } );
+
+            return (
+                <div className={ className }>
+                    { ! RichText.isEmpty( caption ) ? (
+                        <div className="ghostkit-progress-caption">
+                            <RichText.Content value={ caption } />
+                        </div>
+                    ) : '' }
+                    { showCount ? (
+                        <div className="ghostkit-progress-bar-count" style={ { width: `${ percent }%` } }>
+                            <div>
+                                <span>{ countPrefix }</span>
+                                <span>{ percent }</span>
+                                <span>{ countSuffix }</span>
+                            </div>
+                        </div>
+                    ) : '' }
+                    <div className={ classnames( 'ghostkit-progress-wrap', striped ? 'ghostkit-progress-bar-striped' : '' ) }>
+                        <div className={ classnames( 'ghostkit-progress-bar', animateInViewport ? 'ghostkit-count-up' : '' ) } role="progressbar" style={ { width: `${ percent }%`, height: `${ height }px` } } />
+                    </div>
+                </div>
+            );
+        },
+    }, {
         ghostkit: {
             customStylesCallback( attributes ) {
                 const styles = {
