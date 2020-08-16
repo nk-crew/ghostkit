5813aa2be20f6facbe22d6004998e5af83dd45b6
Nikita <nkdevinfo@gmail.com>
2020-08-16 17:25:12 +0300

    fixed heading numbered styles in editor WP 5.5

--- src/gutenberg/style-variants/core-heading/index.js
+++ src/gutenberg/style-variants/core-heading/index.js
@@ -5,36 +5,7 @@ const { __ } = wp.i18n;
 
 const { registerBlockStyle } = wp.blocks;
 
-const { addFilter } = wp.hooks;
-
-const { createHigherOrderComponent } = wp.compose;
-
 registerBlockStyle( 'core/heading', {
     name: 'numbered',
     label: __( 'Numbered', '@@text_domain' ),
 } );
-
-const ghostkitHeadingNumbered = createHigherOrderComponent( ( BlockListBlock ) => ( props ) => {
-    const {
-        attributes,
-        name,
-    } = props;
-
-    const {
-        level,
-        className,
-    } = attributes;
-
-    if ( 'core/heading' !== name || ! /is-style-numbered/.test( className ) ) {
-        return <BlockListBlock { ...props } />;
-    }
-
-    return (
-        <BlockListBlock
-            { ...props }
-            className={ `core-heading-level-${ level }` }
-        />
-    );
-}, 'ghostkitHeadingNumbered' );
-
-addFilter( 'editor.BlockListBlock', 'ghostkit/heading/numbered', ghostkitHeadingNumbered );
