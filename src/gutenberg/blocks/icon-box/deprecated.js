d26edb823eb7656e360391c7d8c27a77621a9624
Nikita <nkdevinfo@gmail.com>
2020-10-09 22:17:45 +0300

    removed outdated blocks deprecations

--- src/gutenberg/blocks/icon-box/deprecated.js
+++ src/gutenberg/blocks/icon-box/deprecated.js
@@ -1,19 +1,8 @@
 /**
- * WordPress dependencies
- */
-/**
  * Internal dependencies
  */
 import metadata from './block.json';
 import save from './save';
-
-const {
-    applyFilters,
-} = wp.hooks;
-
-const {
-    InnerBlocks,
-} = wp.blockEditor;
 
 export default [
     // v2.10.2
@@ -117,69 +106,5 @@ export default [
             },
         },
         save,
-    },
-
-    // v1.0.0
-    {
-        ghostkit: {
-            supports: {
-                styles: true,
-                spacings: true,
-                display: true,
-                scrollReveal: true,
-            },
-        },
-        supports: {
-            html: false,
-            align: [ 'wide', 'full' ],
-        },
-        attributes: {
-            icon: {
-                type: 'string',
-                default: 'fab fa-wordpress-simple',
-            },
-            iconPosition: {
-                type: 'string',
-                default: 'left',
-            },
-            iconSize: {
-                type: 'number',
-                default: 30,
-            },
-            iconColor: {
-                type: 'string',
-                default: '#016c91',
-            },
-        },
-        save( props ) {
-            const {
-                icon,
-                iconPosition,
-            } = props.attributes;
-
-            let {
-                className,
-            } = props;
-
-            className = applyFilters( 'ghostkit.blocks.className', className, {
-                ...{
-                    name: 'ghostkit/icon-box',
-                },
-                ...props,
-            } );
-
-            return (
-                <div className={ className }>
-                    { icon ? (
-                        <div className={ `ghostkit-icon-box-icon ghostkit-icon-box-icon-align-${ iconPosition || 'left' }` }>
-                            <span className={ icon } />
-                        </div>
-                    ) : '' }
-                    <div className="ghostkit-icon-box-content">
-                        <InnerBlocks.Content />
-                    </div>
-                </div>
-            );
-        },
     },
 ];
