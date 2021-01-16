b99e78206fed9b505d5f4852164c972409eb2e08
Nikita <nkdevinfo@gmail.com>
2021-01-16 13:49:50 +0300

    added gutenberg-react-select-styles dependency
    changed Typography select and input styles

--- src/gutenberg/components/select/index.js
+++ src/gutenberg/components/select/index.js
@@ -3,6 +3,7 @@
  */
 // eslint-disable-next-line import/no-unresolved
 import Select from 'react-select-virtualized';
+import selectStyles from 'gutenberg-react-select-styles';
 import classnames from 'classnames/dedupe';
 
 /**
@@ -24,52 +25,7 @@ export default class SelectComponent extends Component {
 
         return (
             <Select
-                styles={ {
-                    control: ( styles, state ) => ( {
-                        ...styles,
-                        minHeight: 32,
-                        ...state.isFocused && ! state.isDisabled ? {
-                            borderColor: '#00a0d2',
-                        } : {},
-                        ...state.isFocused ? {
-                            boxShadow: '0 0 0 1px #00a0d2',
-                            '&:hover': {
-                                borderColor: '#00a0d2',
-                            },
-                        } : {},
-                    } ),
-                    input: ( styles ) => ( {
-                        ...styles,
-                        margin: 0,
-                    } ),
-                    dropdownIndicator: ( styles ) => ( {
-                        ...styles,
-                        padding: 5,
-                        svg: {
-                            width: 15,
-                            height: 15,
-                        },
-                    } ),
-                    clearIndicator: ( styles ) => ( {
-                        ...styles,
-                        padding: 5,
-                        svg: {
-                            width: 15,
-                            height: 15,
-                        },
-                    } ),
-                    multiValue: ( styles ) => ( {
-                        ...styles,
-                    } ),
-                    multiValueLabel: ( styles ) => ( {
-                        ...styles,
-                        padding: 0,
-                    } ),
-                    multiValueRemove: ( styles ) => ( {
-                        ...styles,
-                        padding: 0,
-                    } ),
-                } }
+                styles={ selectStyles }
                 { ...props }
                 className={ classnames( props.className, 'ghostkit-control-select' ) }
             />
