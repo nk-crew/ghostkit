04264497ec0599b05408c84889f1248ea9cfce56
Nikita <nkdevinfo@gmail.com>
2021-01-18 22:17:31 +0300

    added info about custom fonts on Fonts setting page

--- src/settings/pages/fonts.js
+++ src/settings/pages/fonts.js
@@ -17,7 +17,7 @@ export default class FontsSettings extends Component {
         return (
             <ApplyFilters name="ghostkit.fonts.settings" props={ this.props }>
                 <div className="ghostkit-settings-content-wrapper ghostkit-settings-fonts">
-                    { __( 'Adobe Fonts available for PRO users only. Read more about Ghost Kit PRO plugin here - ', '@@text_domain' ) }
+                    { __( 'Adobe and Custom Fonts available for PRO users only. Read more about Ghost Kit PRO plugin here - ', '@@text_domain' ) }
                     <ExternalLink href="https://ghostkit.io/pricing/">https://ghostkit.io/pricing/</ExternalLink>
                 </div>
             </ApplyFilters>
