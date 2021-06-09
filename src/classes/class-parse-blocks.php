c20616be0f9c3791bf2a964d2e12ba0e70088e3c
Nikita <nkdevinfo@gmail.com>
2021-06-09 22:24:53 +0300

    fixed rare conflict with reusable blocks while parsing

--- src/classes/class-parse-blocks.php
+++ src/classes/class-parse-blocks.php
@@ -124,7 +124,7 @@ class GhostKit_Parse_Blocks {
 
                     $reusable_block = get_post( $block['attrs']['ref'] );
 
-                    if ( has_blocks( $reusable_block ) ) {
+                    if ( has_blocks( $reusable_block ) && isset( $reusable_block->post_content ) ) {
                         $post_blocks = parse_blocks( $reusable_block->post_content );
                         self::parse_blocks( $post_blocks, $location, true );
                     }
