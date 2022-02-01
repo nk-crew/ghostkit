7ebd45883d8acb07a30e11562aff983116a98fc9
Nikita <nkdevinfo@gmail.com>
2022-02-01 15:33:19 +0300

    added prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,80 @@
-const md5 = require( 'md5' );
+const md5 = require('md5');
 
-module.exports = function() {
-    return {
-        module: {
-            rules: [
-                {
-                    test: /(\.js)$/,
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true,
-                        cacheCompression: false,
+module.exports = function () {
+  return {
+    module: {
+      rules: [
+        {
+          test: /(\.js)$/,
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader', // creates style nodes from JS strings
+            },
+            {
+              loader: 'css-loader', // translates CSS into CommonJS
+              options: {
+                url: false,
+              },
+            },
+            {
+              loader: 'sass-loader', // compiles Sass to CSS
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader', // creates style nodes from JS strings
+            },
+            {
+              loader: 'css-loader', // translates CSS into CommonJS
+              options: {
+                url: false,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: ({ resource }) => ({
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    removeViewBox: false,
+                  },
+                  {
+                    cleanupIDs: {
+                      prefix: `ghostkit-${md5(resource)}-`,
                     },
-                }, {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: 'style-loader', // creates style nodes from JS strings
-                        }, {
-                            loader: 'css-loader', // translates CSS into CommonJS
-                            options: {
-                                url: false,
-                            },
-                        }, {
-                            loader: 'sass-loader', // compiles Sass to CSS
-                        },
-                    ],
-                }, {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader', // creates style nodes from JS strings
-                        }, {
-                            loader: 'css-loader', // translates CSS into CommonJS
-                            options: {
-                                url: false,
-                            },
-                        },
-                    ],
-                }, {
-                    test: /\.svg$/,
-                    use: ( { resource } ) => ( {
-                        loader: '@svgr/webpack',
-                        options: {
-                            svgoConfig: {
-                                plugins: [
-                                    {
-                                        removeViewBox: false,
-                                    },
-                                    {
-                                        cleanupIDs: {
-                                            prefix: `ghostkit-${ md5( resource ) }-`,
-                                        },
-                                    },
-                                ],
-                            },
-                        },
-                    } ),
-                }, {
-                    test: /\.(gif|png|jpe?g)$/i,
-                    loader: 'base64-inline-loader',
-                },
-            ],
+                  },
+                ],
+              },
+            },
+          }),
         },
-        resolve: {
-            extensions: [ '.js', '.json' ],
+        {
+          test: /\.(gif|png|jpe?g)$/i,
+          loader: 'base64-inline-loader',
         },
-        externals: {
-            react: 'React',
-            'react-dom': 'ReactDOM',
-        },
-    };
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.json'],
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+  };
 };
--- wpeg.config.js
+++ wpeg.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require( 'path' );
+const path = require('path');
 
-const pkg = require( 'json-file' ).read( './package.json' ).data;
+const pkg = require('json-file').read('./package.json').data;
 
 const cfg = {};
 
@@ -13,78 +13,75 @@ cfg.dist = '{dist_root}/{name}';
 
 // Browser sync.
 cfg.browser_sync = {
-    proxy: '{name}.local',
+  proxy: '{name}.local',
 };
 
 // Template variables that will be automatically replaced.
 cfg.template_files_src = '{dist}/**/*.{md,php,js,css,pot,json}';
 cfg.template_files_variables = {
-    text_domain: pkg.name,
-    plugin_version: pkg.version,
-    plugin_name: pkg.name,
-    plugin_title: pkg.title,
-    plugin_author: pkg.author,
+  text_domain: pkg.name,
+  plugin_version: pkg.version,
+  plugin_name: pkg.name,
+  plugin_title: pkg.title,
+  plugin_author: pkg.author,
 };
 
 // Copy files.
 cfg.copy_files_src = [
-    '{src}/**/*',
-    '!{src}/**/*.{js}',
-    '{src}/**/vendor/**/*.{js}',
-    './node_modules/*gist-simple/dist/gist-simple.css',
-    './node_modules/*gist-simple/dist/gist-simple.min.js',
-    './node_modules/*gist-simple/dist/gist-simple.min.js.map',
-    './node_modules/*gmaps/gmaps.min.js',
-    './node_modules/*gmaps/gmaps.min.js.map',
-    './node_modules/*ie11-custom-properties/ie11CustomProperties.js',
-    './node_modules/*jarallax/dist/jarallax-video.min.js',
-    './node_modules/*jarallax/dist/jarallax-video.min.js.map',
-    './node_modules/*jarallax/dist/jarallax.min.js',
-    './node_modules/*jarallax/dist/jarallax.min.js.map',
-    './node_modules/*jarallax/dist/jarallax.css',
-    './node_modules/*object-fit-images/dist/ofi.min.js',
-    './node_modules/*parsleyjs/dist/parsley.min.js',
-    './node_modules/*parsleyjs/dist/parsley.min.js.map',
-    './node_modules/*scrollreveal/dist/scrollreveal.min.js',
-    './node_modules/*swiper/swiper-bundle.min.js',
-    './node_modules/*swiper/swiper-bundle.min.js.map',
-    './node_modules/*swiper/swiper-bundle.min.css',
+  '{src}/**/*',
+  '!{src}/**/*.{js}',
+  '{src}/**/vendor/**/*.{js}',
+  './node_modules/*gist-simple/dist/gist-simple.css',
+  './node_modules/*gist-simple/dist/gist-simple.min.js',
+  './node_modules/*gist-simple/dist/gist-simple.min.js.map',
+  './node_modules/*gmaps/gmaps.min.js',
+  './node_modules/*gmaps/gmaps.min.js.map',
+  './node_modules/*ie11-custom-properties/ie11CustomProperties.js',
+  './node_modules/*jarallax/dist/jarallax-video.min.js',
+  './node_modules/*jarallax/dist/jarallax-video.min.js.map',
+  './node_modules/*jarallax/dist/jarallax.min.js',
+  './node_modules/*jarallax/dist/jarallax.min.js.map',
+  './node_modules/*jarallax/dist/jarallax.css',
+  './node_modules/*object-fit-images/dist/ofi.min.js',
+  './node_modules/*parsleyjs/dist/parsley.min.js',
+  './node_modules/*parsleyjs/dist/parsley.min.js.map',
+  './node_modules/*scrollreveal/dist/scrollreveal.min.js',
+  './node_modules/*swiper/swiper-bundle.min.js',
+  './node_modules/*swiper/swiper-bundle.min.js.map',
+  './node_modules/*swiper/swiper-bundle.min.css',
 ];
 
-cfg.copy_files_dist = ( file ) => {
-    let destPath = `${ cfg.dist_root }/${ cfg.name }`;
-    const filePath = path.relative( process.cwd(), file.path );
+cfg.copy_files_dist = (file) => {
+  let destPath = `${cfg.dist_root}/${cfg.name}`;
+  const filePath = path.relative(process.cwd(), file.path);
 
-    if ( filePath && /^node_modules/g.test( filePath ) ) {
-        destPath += '/assets/vendor';
-    }
+  if (filePath && /^node_modules/g.test(filePath)) {
+    destPath += '/assets/vendor';
+  }
 
-    return destPath;
+  return destPath;
 };
 
 // Prefix SCSS files.
-cfg.prefix_scss_files_src = [
-    '{dist}/**/*.scss',
-    '!{dist}/**/vendor/**/*.{scss}',
-];
+cfg.prefix_scss_files_src = ['{dist}/**/*.scss', '!{dist}/**/vendor/**/*.{scss}'];
 
 // Compile SCSS files.
 cfg.compile_scss_files_src = [
-    '{src}/*assets/**/*.scss',
-    '{src}/*gutenberg/style.scss',
-    '{src}/*gutenberg/editor.scss',
-    '{src}/*gutenberg/blocks/*/styles/style.scss',
-    '{src}/*settings/style.scss',
+  '{src}/*assets/**/*.scss',
+  '{src}/*gutenberg/style.scss',
+  '{src}/*gutenberg/editor.scss',
+  '{src}/*gutenberg/blocks/*/styles/style.scss',
+  '{src}/*settings/style.scss',
 ];
 cfg.compile_scss_files_rtl = true;
 
 // Compile JS files.
 cfg.compile_js_files_src = [
-    '{src}/*assets/**/*.js',
-    '{src}/*gutenberg/index.js',
-    '{src}/*gutenberg/blocks/*/frontend.js',
-    '{src}/*settings/index.js',
-    '!{src}/**/vendor/**/*',
+  '{src}/*assets/**/*.js',
+  '{src}/*gutenberg/index.js',
+  '{src}/*gutenberg/blocks/*/frontend.js',
+  '{src}/*settings/index.js',
+  '!{src}/**/vendor/**/*',
 ];
 
 // Correct line endings files.
@@ -92,19 +89,19 @@ cfg.correct_line_endings_files_src = '{dist}/**/*.{js,css}';
 
 // ZIP files.
 cfg.zip_files = [
-    {
-        src: '{dist}/**/*',
-        src_opts: {
-            base: '{dist_root}',
-        },
-        dist: '{dist_root}/{name}.zip',
+  {
+    src: '{dist}/**/*',
+    src_opts: {
+      base: '{dist_root}',
     },
+    dist: '{dist_root}/{name}.zip',
+  },
 ];
 
 // Watch files.
-cfg.watch_files = [ '{src}/**/*', '!{src}/**/*.{js}' ];
+cfg.watch_files = ['{src}/**/*', '!{src}/**/*.{js}'];
 
-cfg.watch_js_files = [ '{src}/**/*.js', '{src}/**/*.json', '!{src}/*vendor/**/*' ];
+cfg.watch_js_files = ['{src}/**/*.js', '{src}/**/*.json', '!{src}/*vendor/**/*'];
 
 cfg.watch_scss_files = '{src}/**/*.scss';
 
