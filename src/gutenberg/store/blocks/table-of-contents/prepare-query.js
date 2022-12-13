c32ad5d39e4f5296513d4f3330f776201d847b5f
Nikita <nkdevinfo@gmail.com>
2022-12-13 15:01:02 +0300

    fixed TOC conflict with special characters in headings (resolves #162)

--- src/gutenberg/store/blocks/table-of-contents/prepare-query.js
+++ src/gutenberg/store/blocks/table-of-contents/prepare-query.js
@@ -4,7 +4,12 @@
 import qs from 'qs';
 
 export default function prepareQuery(data) {
-  const additionalData = qs.stringify(data, { encode: false });
+  /**
+   * We should encode it, because request may not work correctly with special characters.
+   * For example there was a problem with `#` character in the heading text.
+   * @link https://github.com/nk-crew/ghostkit/issues/162
+   */
+  const additionalData = qs.stringify(data, { encode: true });
   const query = `/ghostkit/v1/get_table_of_contents/${additionalData ? `?${additionalData}` : ''}`;
 
   return query;
