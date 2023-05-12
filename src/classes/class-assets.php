523ae5c3ef7a6d5c2f3595302c7b2f090b46645f
Alexey <alexey.fellan@gmail.com>
2023-05-12 17:51:35 +0300

    fixed typographyExist localize setting

--- src/classes/class-assets.php
+++ src/classes/class-assets.php
@@ -347,7 +347,7 @@ class GhostKit_Assets {
                 'themeName'                   => $theme_data->get( 'Name' ),
                 'isFseTheme'                  => current_theme_supports( 'block-templates' ) ? true : false,
                 'fontsApiExist'               => class_exists( 'WP_Fonts' ),
-                'typographyExist'             => GhostKit_Typography::typography_exist() ? true : false,
+                'typographyExist'             => GhostKit_Typography::typography_exist(),
                 'settings'                    => get_option( 'ghostkit_settings', array() ),
                 'disabledBlocks'              => get_option( 'ghostkit_disabled_blocks', array() ),
 
