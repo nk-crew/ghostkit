ac9b3a81377bee518420c8844cb1ff1885c4a1dd
dexad <37379331+dexad@users.noreply.github.com>
2023-06-28 11:46:46 +0300

    Removed jquery from Alert

--- src/gutenberg/blocks/alert/frontend.js
+++ src/gutenberg/blocks/alert/frontend.js
@@ -2,29 +2,29 @@
  * Block Alert
  */
 const {
-  jQuery: $,
   Motion: { animate },
   GHOSTKIT,
 } = window;
 
-const $doc = $(document);
-
 // Dismiss button.
-$doc.on('click', '.ghostkit-alert-hide-button', function (e) {
-  e.preventDefault();
+document.querySelectorAll('.ghostkit-alert-hide-button').forEach(($button) => {
+  const handler = (e) => {
+    e.preventDefault();
 
-  const alert = this.parentNode;
-  const $alert = $(alert);
+    const alert = $button.parentNode;
 
-  animate(alert, { opacity: 0 }, { duration: 0.5 }).finished.then(() => {
-    alert.style.height = `${alert.offsetHeight}px`;
-    alert.style.paddingTop = '0px';
-    alert.style.paddingBottom = '0px';
+    animate(alert, { opacity: 0 }, { duration: 0.5 }).finished.then(() => {
+      alert.style.height = `${alert.offsetHeight}px`;
+      alert.style.paddingTop = '0px';
+      alert.style.paddingBottom = '0px';
 
-    animate(alert, { height: 0, marginTop: 0, marginBottom: 0 }, { duration: 0.5 }).finished.then(
-      () => {
-        GHOSTKIT.triggerEvent('dismissedAlert', GHOSTKIT.classObject, $alert);
-      }
-    );
-  });
+      animate(alert, { height: 0, marginTop: 0, marginBottom: 0 }, { duration: 0.5 }).finished.then(
+        () => {
+          GHOSTKIT.triggerEvent('dismissedAlert', GHOSTKIT.classObject, alert);
+        }
+      );
+    });
+  };
+
+  $button.addEventListener('click', handler);
 });
