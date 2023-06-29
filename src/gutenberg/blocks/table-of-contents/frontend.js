e93c81465f37c1ed5f162881122e28aafaee61d3
dexad <37379331+dexad@users.noreply.github.com>
2023-06-29 08:12:10 +0300

    Removed jquery from Table of Contents

--- src/gutenberg/blocks/table-of-contents/frontend.js
+++ src/gutenberg/blocks/table-of-contents/frontend.js
@@ -6,57 +6,56 @@ import { maybeDecode } from '../../utils/encode-decode';
 /**
  * Block TOC
  */
-const { jQuery: $ } = window;
-const $doc = $(document);
-const $html = $('html');
+const $html = document.documentElement;
 
 /**
  * Prepare TOCs click to link.
  */
 function initSmoothScroll() {
-  $doc.on('click', '.ghostkit-toc a', (evt) => {
-    evt.preventDefault();
+  document.querySelectorAll('.ghostkit-toc a').forEach(($link) => {
+    const handler = (evt) => {
+      evt.preventDefault();
 
-    if (!evt.target || !evt.target.hash) {
-      return;
-    }
-
-    const offset = $(maybeDecode(evt.target.hash)).offset();
-
-    if (typeof offset === 'undefined') {
-      return;
-    }
-
-    let { top } = offset;
-
-    // Get offset from CSS.
-    const scrollPadding = parseFloat($html.css('scroll-padding-top'));
-
-    if (scrollPadding) {
-      top -= scrollPadding;
-    } else {
-      const $adminBar = $('#wpadminbar');
-
-      // Admin bar offset.
-      if ($adminBar.length && $adminBar.css('position') === 'fixed') {
-        top -= $adminBar.outerHeight();
+      if (!evt.target || !evt.target.hash) {
+        return;
       }
-    }
 
-    // Limit max offset.
-    top = Math.max(0, top);
+      const offsetEl = document.getElementById(maybeDecode(evt.target.hash).substring(1));
 
-    window.scrollTo({
-      top,
-      behavior: 'smooth',
-    });
+      if (!offsetEl) {
+        return;
+      }
+
+      let { top } = offsetEl.getBoundingClientRect();
+
+      // Get offset from CSS.
+      const scrollPadding = parseFloat(getComputedStyle($html)['scroll-padding-top']);
+
+      if (scrollPadding) {
+        top -= scrollPadding;
+      } else {
+        const $adminBar = document.getElementById('wpadminbar');
+
+        // Admin bar offset.
+        if ($adminBar && getComputedStyle($adminBar).position === 'fixed') {
+          top -= $adminBar.getBoundingClientRect().height;
+        }
+      }
+
+      // Limit max offset.
+      top = Math.max(0, top);
+
+      window.scrollTo({
+        top,
+        behavior: 'smooth',
+      });
+    };
+
+    $link.addEventListener('click', handler);
   });
 }
 
 // If smooth scroll enabled in CSS, we don't need to run it with JS.
-if (
-  !('scrollBehavior' in document.documentElement.style) ||
-  $html.css('scroll-behavior') !== 'smooth'
-) {
+if (!('scrollBehavior' in $html.style) || getComputedStyle($html)['scroll-behavior'] !== 'smooth') {
   initSmoothScroll();
 }
