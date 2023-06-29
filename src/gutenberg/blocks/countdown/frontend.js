ccf47e3ec6bf4c585d58c05730d0db1a2bd1fed3
dexad <37379331+dexad@users.noreply.github.com>
2023-06-29 06:48:17 +0300

    Removed jquery from Countdown
    The Jquery `initBlocks.ghostkit` event remains

--- src/gutenberg/blocks/countdown/frontend.js
+++ src/gutenberg/blocks/countdown/frontend.js
@@ -21,8 +21,11 @@ $doc.on('initBlocks.ghostkit', (e, self) => {
     const isEnd = dateData.value >= 0;
 
     if (isEnd) {
-      $this.children('.ghostkit-countdown-unit').hide();
-      $this.children('.ghostkit-countdown-expire-action').show();
+      $this.querySelectorAll(':scope > .ghostkit-countdown-unit').forEach(($unit) => {
+        $unit.style.display = 'none';
+      });
+
+      $this.querySelector(':scope > .ghostkit-countdown-expire-action').style.display = 'block';
       return;
     }
 
@@ -36,11 +39,11 @@ $doc.on('initBlocks.ghostkit', (e, self) => {
       const newNumber = formattedUnit ? formattedUnit.number : '00';
       const newLabel = formattedUnit ? formattedUnit.label : unitName;
 
-      if (unitsElements[unitName].$number.html() !== newNumber) {
-        unitsElements[unitName].$number.html(newNumber);
+      if (unitsElements[unitName].$number.innerHTML !== newNumber) {
+        unitsElements[unitName].$number.innerHTML = newNumber;
       }
-      if (unitsElements[unitName].$label.html() !== newLabel) {
-        unitsElements[unitName].$label.html(newLabel);
+      if (unitsElements[unitName].$label.innerHTML !== newLabel) {
+        unitsElements[unitName].$label.innerHTML = newLabel;
       }
     });
 
@@ -51,31 +54,32 @@ $doc.on('initBlocks.ghostkit', (e, self) => {
 
   GHOSTKIT.triggerEvent('beforePrepareCountdown', self);
 
-  $('.ghostkit-countdown:not(.ghostkit-countdown-ready)').each(function () {
-    const $this = $(this);
-    $this.addClass('ghostkit-countdown-ready');
+  document
+    .querySelectorAll('.ghostkit-countdown:not(.ghostkit-countdown-ready)')
+    .forEach(($countdown) => {
+      $countdown.classList.add('ghostkit-countdown-ready');
 
-    const date = new Date($this.attr('data-date'));
+      const date = new Date($countdown.getAttribute('data-date'));
 
-    const unitsElements = [];
-    const units = ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'].filter(
-      (unitName) => {
-        const $unit = $this.children(`.ghostkit-countdown-unit-${unitName}`);
+      const unitsElements = [];
+      const units = ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'].filter(
+        (unitName) => {
+          const $unit = $countdown.querySelector(`:scope > .ghostkit-countdown-unit-${unitName}`);
 
-        if ($unit.length) {
-          unitsElements[unitName] = {
-            $number: $unit.find('.ghostkit-countdown-unit-number'),
-            $label: $unit.find('.ghostkit-countdown-unit-label'),
-          };
-          return true;
+          if ($unit) {
+            unitsElements[unitName] = {
+              $number: $unit.querySelector('.ghostkit-countdown-unit-number'),
+              $label: $unit.querySelector('.ghostkit-countdown-unit-label'),
+            };
+            return true;
+          }
+
+          return false;
         }
+      );
 
-        return false;
-      }
-    );
-
-    updateUnits(date, units, unitsElements, $this);
-  });
+      updateUnits(date, units, unitsElements, $countdown);
+    });
 
   GHOSTKIT.triggerEvent('afterPrepareCountdown', self);
 });
