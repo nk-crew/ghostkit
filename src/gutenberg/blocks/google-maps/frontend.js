3db797838e364749f7b9581ddfb8f02420bb9c67
dexad <37379331+dexad@users.noreply.github.com>
2023-06-30 09:55:18 +0300

    Removed jquery from GoogleMap
    The jQuery `initBlocks.ghostkit` event remains

--- src/gutenberg/blocks/google-maps/frontend.js
+++ src/gutenberg/blocks/google-maps/frontend.js
@@ -13,75 +13,81 @@ $doc.on('initBlocks.ghostkit', (e, self) => {
   if (GHOSTKIT.googleMapsLibrary && GHOSTKIT.googleMapsAPIKey) {
     GHOSTKIT.triggerEvent('beforePrepareGoogleMaps', self);
 
-    $('.ghostkit-google-maps:not(.ghostkit-google-maps-ready)').each(function () {
-      const $this = $(this);
-      $this.addClass('ghostkit-google-maps-ready');
+    document
+      .querySelectorAll('.ghostkit-google-maps:not(.ghostkit-google-maps-ready)')
+      .forEach(($this) => {
+        $this.classList.add('ghostkit-google-maps-ready');
 
-      scriptjs(`${GHOSTKIT.googleMapsAPIUrl}&key=${GHOSTKIT.googleMapsAPIKey}`, () => {
-        scriptjs(GHOSTKIT.googleMapsLibrary.url, () => {
-          GHOSTKIT.triggerEvent('beforePrepareGoogleMapsStart', self, $this);
+        scriptjs(`${GHOSTKIT.googleMapsAPIUrl}&key=${GHOSTKIT.googleMapsAPIKey}`, () => {
+          scriptjs(GHOSTKIT.googleMapsLibrary.url, () => {
+            GHOSTKIT.triggerEvent('beforePrepareGoogleMapsStart', self, $this);
 
-          let styles = '';
-          let markers = '';
+            let styles = '';
+            let markers = '';
 
-          try {
-            styles = JSON.parse($this.attr('data-styles'));
-            // eslint-disable-next-line no-empty
-          } catch (evt) {}
-
-          const $markers = $this.find('.ghostkit-google-maps-marker');
-          if ($markers.length) {
-            markers = [];
-
-            $markers.each(function () {
-              markers.push($(this).data());
-            });
-
-            // old way.
-          } else if ($this.attr('data-markers')) {
             try {
-              markers = JSON.parse($this.attr('data-markers'));
+              styles = JSON.parse($this.getAttribute('data-styles'));
               // eslint-disable-next-line no-empty
             } catch (evt) {}
-          }
 
-          const opts = {
-            div: $this[0],
-            lat: parseFloat($this.attr('data-lat')),
-            lng: parseFloat($this.attr('data-lng')),
-            zoom: parseInt($this.attr('data-zoom'), 10),
-            zoomControl: $this.attr('data-show-zoom-buttons') === 'true',
-            zoomControlOpt: {
-              style: 'DEFAULT',
-              position: 'RIGHT_BOTTOM',
-            },
-            mapTypeControl: $this.attr('data-show-map-type-buttons') === 'true',
-            streetViewControl: $this.attr('data-show-street-view-button') === 'true',
-            fullscreenControl: $this.attr('data-show-fullscreen-button') === 'true',
-            scrollwheel: $this.attr('data-option-scroll-wheel') === 'true',
-            draggable: $this.attr('data-option-draggable') === 'true',
-            styles,
-          };
+            const $markers = $this.querySelectorAll('.ghostkit-google-maps-marker');
+            if ($markers.length) {
+              markers = [];
 
-          const mapObject = new window.GMaps(opts);
+              $markers.forEach(($marker) => {
+                const markerConf = {};
 
-          // add gestureHandling
-          const gestureHandling = $this.attr('data-gesture-handling');
-          if (mapObject && gestureHandling === 'cooperative') {
-            mapObject.setOptions({
-              gestureHandling,
-              scrollwheel: opts.scrollwheel ? null : opts.scrollwheel,
-            });
-          }
+                markerConf.lat = $marker.getAttribute('data-lat');
+                markerConf.lng = $marker.getAttribute('data-lng');
 
-          if (markers && markers.length) {
-            mapObject.addMarkers(markers);
-          }
+                markers.push(markerConf);
+              });
 
-          GHOSTKIT.triggerEvent('beforePrepareGoogleMapsEnd', self, $this, mapObject);
+              // old way.
+            } else if ($this.getAttribute('data-markers')) {
+              try {
+                markers = JSON.parse($this.getAttribute('data-markers'));
+                // eslint-disable-next-line no-empty
+              } catch (evt) {}
+            }
+
+            const opts = {
+              div: $this,
+              lat: parseFloat($this.getAttribute('data-lat')),
+              lng: parseFloat($this.getAttribute('data-lng')),
+              zoom: parseInt($this.getAttribute('data-zoom'), 10),
+              zoomControl: $this.getAttribute('data-show-zoom-buttons') === 'true',
+              zoomControlOpt: {
+                style: 'DEFAULT',
+                position: 'RIGHT_BOTTOM',
+              },
+              mapTypeControl: $this.getAttribute('data-show-map-type-buttons') === 'true',
+              streetViewControl: $this.getAttribute('data-show-street-view-button') === 'true',
+              fullscreenControl: $this.getAttribute('data-show-fullscreen-button') === 'true',
+              scrollwheel: $this.getAttribute('data-option-scroll-wheel') === 'true',
+              draggable: $this.getAttribute('data-option-draggable') === 'true',
+              styles,
+            };
+
+            const mapObject = new window.GMaps(opts);
+
+            // add gestureHandling
+            const gestureHandling = $this.getAttribute('data-gesture-handling');
+            if (mapObject && gestureHandling === 'cooperative') {
+              mapObject.setOptions({
+                gestureHandling,
+                scrollwheel: opts.scrollwheel ? null : opts.scrollwheel,
+              });
+            }
+
+            if (markers && markers.length) {
+              mapObject.addMarkers(markers);
+            }
+
+            GHOSTKIT.triggerEvent('beforePrepareGoogleMapsEnd', self, $this, mapObject);
+          });
         });
       });
-    });
 
     GHOSTKIT.triggerEvent('afterPrepareGoogleMaps', self);
   }
