e88f43b2833dcc7d45e0d3de83ab62d665303cff
dexad <37379331+dexad@users.noreply.github.com>
2023-08-17 15:14:41 +0300

    Rewrote the class into a function

--- src/gutenberg/blocks/carousel/edit.js
+++ src/gutenberg/blocks/carousel/edit.js
@@ -18,15 +18,13 @@ const { applyFilters } = wp.hooks;
 
 const { __ } = wp.i18n;
 
-const { Component, Fragment } = wp.element;
+const { Fragment } = wp.element;
 
 const { PanelBody, ToggleControl } = wp.components;
 
 const { InspectorControls, InnerBlocks } = wp.blockEditor;
 
-const { compose } = wp.compose;
-
-const { withSelect, withDispatch } = wp.data;
+const { useSelect, useDispatch } = wp.data;
 
 const { createBlock } = wp.blocks;
 
@@ -35,21 +33,47 @@ const slideBlockName = 'ghostkit/carousel-slide';
 /**
  * Block Edit Class.
  */
-class BlockEdit extends Component {
-  constructor(props) {
-    super(props);
+export default function BlockEdit(props) {
+  const { attributes, setAttributes } = props;
 
-    this.updateSlidesCount = this.updateSlidesCount.bind(this);
-  }
+  let { className = '' } = props;
+
+  const {
+    effect,
+    speed,
+    autoplay,
+    autoplayHoverPause,
+    slidesPerView,
+    centeredSlides,
+    loop,
+    freeScroll,
+    fadeEdges,
+    showArrows,
+    arrowPrevIcon,
+    arrowNextIcon,
+    showBullets,
+    dynamicBullets,
+    gap,
+  } = attributes;
+
+  const { getBlocks, slidesCount, block } = useSelect((select, ownProps) => {
+    const { clientId } = ownProps;
+
+    return {
+      getBlocks: select('core/block-editor').getBlocks,
+      slidesCount: select('core/block-editor').getBlockCount(clientId),
+      block: select('core/block-editor').getBlock(clientId),
+    };
+  });
+
+  const { removeBlock, replaceInnerBlocks } = useDispatch('core/block-editor');
 
   /**
    * Updates the slides count
    *
    * @param {number} newSlidesCount New slides count.
    */
-  updateSlidesCount(newSlidesCount) {
-    const { block, getBlocks, replaceInnerBlocks, slidesCount, removeBlock } = this.props;
-
+  function updateSlidesCount(newSlidesCount) {
     // Remove slider block.
     if (newSlidesCount < 1) {
       removeBlock(block.clientId);
@@ -74,221 +98,175 @@ class BlockEdit extends Component {
     }
   }
 
-  render() {
-    const { attributes, setAttributes, slidesCount } = this.props;
+  className = classnames(
+    className,
+    'ghostkit-carousel',
+    fadeEdges && 'ghostkit-carousel-fade-edges'
+  );
 
-    let { className = '' } = this.props;
+  className = applyFilters('ghostkit.editor.className', className, props);
 
-    const {
-      effect,
-      speed,
-      autoplay,
-      autoplayHoverPause,
-      slidesPerView,
-      centeredSlides,
-      loop,
-      freeScroll,
-      fadeEdges,
-      showArrows,
-      arrowPrevIcon,
-      arrowNextIcon,
-      showBullets,
-      dynamicBullets,
-      gap,
-    } = attributes;
-
-    className = classnames(
-      className,
-      'ghostkit-carousel',
-      fadeEdges && 'ghostkit-carousel-fade-edges'
-    );
-
-    className = applyFilters('ghostkit.editor.className', className, this.props);
-
-    return (
-      <Fragment>
-        <InspectorControls>
-          <PanelBody>
-            <RangeControl
-              label={__('Slides', '@@text_domain')}
-              value={slidesCount}
-              onChange={(value) => this.updateSlidesCount(value)}
-              min={2}
-              max={20}
-              allowCustomMax
-            />
-          </PanelBody>
-          <PanelBody>
-            <ToggleGroup
-              label={__('Effect', '@@text_domain')}
-              value={effect}
-              options={[
-                {
-                  value: 'slide',
-                  label: __('Slide', '@@text_domain'),
-                },
-                {
-                  value: 'coverflow',
-                  label: __('Coverflow', '@@text_domain'),
-                },
-                {
-                  value: 'fade',
-                  label: __('Fade', '@@text_domain'),
-                },
-              ]}
-              onChange={(value) => {
-                setAttributes({ effect: value });
-              }}
-              isAdaptiveWidth
-            />
-          </PanelBody>
-          <PanelBody>
-            <RangeControl
-              label={__('Speed (seconds)', '@@text_domain')}
-              suffix={__('sec', '@@text_domain')}
-              value={speed}
-              onChange={(value) => setAttributes({ speed: value })}
-              min={0}
-              max={10}
-              step={0.1}
-              allowCustomMax
-            />
-            <RangeControl
-              label={__('Autoplay (seconds)', '@@text_domain')}
-              value={autoplay}
-              onChange={(value) => setAttributes({ autoplay: value })}
-              min={0}
-              max={20}
-              step={0.3}
-              allowCustomMax
-            />
-            {autoplay ? (
-              <ToggleControl
-                label={__('Pause autoplay on mouse over', '@@text_domain')}
-                checked={!!autoplayHoverPause}
-                onChange={(val) => setAttributes({ autoplayHoverPause: val })}
-              />
-            ) : null}
-            {effect !== 'fade' ? (
-              <Fragment>
-                <RangeControl
-                  label={__('Slides per view', '@@text_domain')}
-                  value={slidesPerView}
-                  onChange={(value) => setAttributes({ slidesPerView: value })}
-                  min={1}
-                  max={8}
-                  allowCustomMax
-                />
-                <RangeControl
-                  label={__('Gap', '@@text_domain')}
-                  value={gap}
-                  onChange={(value) => setAttributes({ gap: value })}
-                  min={0}
-                  max={60}
-                  allowCustomMax
-                />
-              </Fragment>
-            ) : (
-              ''
-            )}
-          </PanelBody>
-          <PanelBody>
-            <ToggleControl
-              label={__('Centered slides', '@@text_domain')}
-              checked={!!centeredSlides}
-              onChange={(val) => setAttributes({ centeredSlides: val })}
-            />
-            <ToggleControl
-              label={__('Loop', '@@text_domain')}
-              checked={!!loop}
-              onChange={(val) => setAttributes({ loop: val })}
-            />
-            <ToggleControl
-              label={__('Free scroll', '@@text_domain')}
-              checked={!!freeScroll}
-              onChange={(val) => setAttributes({ freeScroll: val })}
-            />
-            <ToggleControl
-              label={__('Fade edges', '@@text_domain')}
-              checked={!!fadeEdges}
-              onChange={(val) => setAttributes({ fadeEdges: val })}
-            />
-            <ToggleControl
-              label={__('Show arrows', '@@text_domain')}
-              checked={!!showArrows}
-              onChange={(val) => setAttributes({ showArrows: val })}
-            />
-            {showArrows ? (
-              <Fragment>
-                <IconPicker
-                  label={__('Prev arrow icon', '@@text_domain')}
-                  value={arrowPrevIcon}
-                  onChange={(value) => setAttributes({ arrowPrevIcon: value })}
-                />
-                <IconPicker
-                  label={__('Next arrow icon', '@@text_domain')}
-                  value={arrowNextIcon}
-                  onChange={(value) => setAttributes({ arrowNextIcon: value })}
-                />
-              </Fragment>
-            ) : (
-              ''
-            )}
-            <ToggleControl
-              label={__('Show bullets', '@@text_domain')}
-              checked={!!showBullets}
-              onChange={(val) => setAttributes({ showBullets: val })}
-            />
-            {showBullets ? (
-              <ToggleControl
-                label={__('Dynamic bullets', '@@text_domain')}
-                checked={!!dynamicBullets}
-                onChange={(val) => setAttributes({ dynamicBullets: val })}
-              />
-            ) : (
-              ''
-            )}
-          </PanelBody>
-        </InspectorControls>
-        <div className={className}>
-          <InnerBlocks
-            template={[[slideBlockName], [slideBlockName], [slideBlockName]]}
-            allowedBlocks={[slideBlockName]}
-            orientation="horizontal"
-            renderAppender={false}
+  return (
+    <Fragment>
+      <InspectorControls>
+        <PanelBody>
+          <RangeControl
+            label={__('Slides', '@@text_domain')}
+            value={slidesCount}
+            onChange={(value) => updateSlidesCount(value)}
+            min={2}
+            max={20}
+            allowCustomMax
           />
-        </div>
-        <EditorStyles
-          styles={`
-              [data-block="${this.props.clientId}"] > .ghostkit-carousel {
-                --gkt-carousel-gap: ${gap}px;
-                --gkt-carousel-slides-per-view: ${effect === 'fade' ? 1 : slidesPerView};
-              }
-            `}
+        </PanelBody>
+        <PanelBody>
+          <ToggleGroup
+            label={__('Effect', '@@text_domain')}
+            value={effect}
+            options={[
+              {
+                value: 'slide',
+                label: __('Slide', '@@text_domain'),
+              },
+              {
+                value: 'coverflow',
+                label: __('Coverflow', '@@text_domain'),
+              },
+              {
+                value: 'fade',
+                label: __('Fade', '@@text_domain'),
+              },
+            ]}
+            onChange={(value) => {
+              setAttributes({ effect: value });
+            }}
+            isAdaptiveWidth
+          />
+        </PanelBody>
+        <PanelBody>
+          <RangeControl
+            label={__('Speed (seconds)', '@@text_domain')}
+            suffix={__('sec', '@@text_domain')}
+            value={speed}
+            onChange={(value) => setAttributes({ speed: value })}
+            min={0}
+            max={10}
+            step={0.1}
+            allowCustomMax
+          />
+          <RangeControl
+            label={__('Autoplay (seconds)', '@@text_domain')}
+            value={autoplay}
+            onChange={(value) => setAttributes({ autoplay: value })}
+            min={0}
+            max={20}
+            step={0.3}
+            allowCustomMax
+          />
+          {autoplay ? (
+            <ToggleControl
+              label={__('Pause autoplay on mouse over', '@@text_domain')}
+              checked={!!autoplayHoverPause}
+              onChange={(val) => setAttributes({ autoplayHoverPause: val })}
+            />
+          ) : null}
+          {effect !== 'fade' ? (
+            <Fragment>
+              <RangeControl
+                label={__('Slides per view', '@@text_domain')}
+                value={slidesPerView}
+                onChange={(value) => setAttributes({ slidesPerView: value })}
+                min={1}
+                max={8}
+                allowCustomMax
+              />
+              <RangeControl
+                label={__('Gap', '@@text_domain')}
+                value={gap}
+                onChange={(value) => setAttributes({ gap: value })}
+                min={0}
+                max={60}
+                allowCustomMax
+              />
+            </Fragment>
+          ) : (
+            ''
+          )}
+        </PanelBody>
+        <PanelBody>
+          <ToggleControl
+            label={__('Centered slides', '@@text_domain')}
+            checked={!!centeredSlides}
+            onChange={(val) => setAttributes({ centeredSlides: val })}
+          />
+          <ToggleControl
+            label={__('Loop', '@@text_domain')}
+            checked={!!loop}
+            onChange={(val) => setAttributes({ loop: val })}
+          />
+          <ToggleControl
+            label={__('Free scroll', '@@text_domain')}
+            checked={!!freeScroll}
+            onChange={(val) => setAttributes({ freeScroll: val })}
+          />
+          <ToggleControl
+            label={__('Fade edges', '@@text_domain')}
+            checked={!!fadeEdges}
+            onChange={(val) => setAttributes({ fadeEdges: val })}
+          />
+          <ToggleControl
+            label={__('Show arrows', '@@text_domain')}
+            checked={!!showArrows}
+            onChange={(val) => setAttributes({ showArrows: val })}
+          />
+          {showArrows ? (
+            <Fragment>
+              <IconPicker
+                label={__('Prev arrow icon', '@@text_domain')}
+                value={arrowPrevIcon}
+                onChange={(value) => setAttributes({ arrowPrevIcon: value })}
+              />
+              <IconPicker
+                label={__('Next arrow icon', '@@text_domain')}
+                value={arrowNextIcon}
+                onChange={(value) => setAttributes({ arrowNextIcon: value })}
+              />
+            </Fragment>
+          ) : (
+            ''
+          )}
+          <ToggleControl
+            label={__('Show bullets', '@@text_domain')}
+            checked={!!showBullets}
+            onChange={(val) => setAttributes({ showBullets: val })}
+          />
+          {showBullets ? (
+            <ToggleControl
+              label={__('Dynamic bullets', '@@text_domain')}
+              checked={!!dynamicBullets}
+              onChange={(val) => setAttributes({ dynamicBullets: val })}
+            />
+          ) : (
+            ''
+          )}
+        </PanelBody>
+      </InspectorControls>
+      <div className={className}>
+        <InnerBlocks
+          template={[[slideBlockName], [slideBlockName], [slideBlockName]]}
+          allowedBlocks={[slideBlockName]}
+          orientation="horizontal"
+          renderAppender={false}
         />
-      </Fragment>
-    );
-  }
+      </div>
+      <EditorStyles
+        styles={`
+            [data-block="${props.clientId}"] > .ghostkit-carousel {
+              --gkt-carousel-gap: ${gap}px;
+              --gkt-carousel-slides-per-view: ${effect === 'fade' ? 1 : slidesPerView};
+            }
+          `}
+      />
+    </Fragment>
+  );
 }
-
-export default compose([
-  withSelect((select, ownProps) => {
-    const { getBlock, getBlocks, getBlockCount } = select('core/block-editor');
-
-    const { clientId } = ownProps;
-
-    return {
-      getBlocks,
-      slidesCount: getBlockCount(clientId),
-      block: getBlock(clientId),
-    };
-  }),
-  withDispatch((dispatch) => {
-    const { removeBlock, replaceInnerBlocks } = dispatch('core/block-editor');
-
-    return {
-      removeBlock,
-      replaceInnerBlocks,
-    };
-  }),
-])(BlockEdit);
