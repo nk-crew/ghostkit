ba3a7559ed9f89d0fd7aa0461eb45db9d5df5dc6
dexad <37379331+dexad@users.noreply.github.com>
2023-09-10 09:18:20 +0300

    Added border icons

--- src/gutenberg/icons/index.js
+++ src/gutenberg/icons/index.js
@@ -41,6 +41,10 @@ import blockFormFieldSelect from './block-form-field-select.svg';
 import blockFormFieldText from './block-form-field-text.svg';
 import blockFormFieldTextarea from './block-form-field-textarea.svg';
 import blockFormFieldUrl from './block-form-field-url.svg';
+import borderDashed from './border-dashed.svg';
+import borderDotted from './border-dotted.svg';
+import borderDouble from './border-double.svg';
+import borderSolid from './border-solid.svg';
 import pluginGhostkit from './plugin-ghostkit.svg';
 import pluginTemplates from './plugin-templates.svg';
 import pluginCustomCode from './plugin-custom-code.svg';
@@ -142,6 +146,11 @@ export default {
   'block-form-field-text': blockFormFieldText,
   'block-form-field-textarea': blockFormFieldTextarea,
   'block-form-field-url': blockFormFieldUrl,
+
+  'border-dashed': borderDashed,
+  'border-dotted': borderDotted,
+  'border-double': borderDouble,
+  'border-solid': borderSolid,
 
   'plugin-ghostkit': pluginGhostkit,
   'plugin-templates': pluginTemplates,
