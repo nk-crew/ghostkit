a9bc9674ea9c4277e58c37bc97e409eccc0559ce
dexad <37379331+dexad@users.noreply.github.com>
2023-09-10 08:03:58 +0300

    Fixed block props in the Changelog

--- src/gutenberg/blocks/changelog/save.js
+++ src/gutenberg/blocks/changelog/save.js
@@ -8,46 +8,48 @@ import metadata from './block.json';
 
 const { applyFilters } = wp.hooks;
 
-const { Component } = wp.element;
+const {
+  RichText,
+  useBlockProps,
+  useInnerBlocksProps: __stableUseInnerBlocksProps,
+  __experimentalUseInnerBlocksProps,
+} = wp.blockEditor;
 
-const { InnerBlocks, RichText } = wp.blockEditor;
+const useInnerBlocksProps = __stableUseInnerBlocksProps || __experimentalUseInnerBlocksProps;
 
 const { name } = metadata;
 
 /**
  * Block Save Class.
  */
-class BlockSave extends Component {
-  render() {
-    const { version, date } = this.props.attributes;
+export default function BlockSave(props) {
+  const { version, date } = props.attributes;
 
-    let className = 'ghostkit-changelog';
+  let className = 'ghostkit-changelog';
 
-    className = applyFilters('ghostkit.blocks.className', className, {
-      ...{
-        name,
-      },
-      ...this.props,
-    });
+  className = applyFilters('ghostkit.blocks.className', className, {
+    ...{
+      name,
+    },
+    ...props,
+  });
 
-    return (
-      <div className={className}>
-        {!RichText.isEmpty(version) ? (
-          <RichText.Content tagName="span" className="ghostkit-changelog-version" value={version} />
-        ) : (
-          ''
-        )}
-        {!RichText.isEmpty(date) ? (
-          <RichText.Content tagName="h2" className="ghostkit-changelog-date" value={date} />
-        ) : (
-          ''
-        )}
-        <div className="ghostkit-changelog-more">
-          <InnerBlocks.Content />
-        </div>
-      </div>
-    );
-  }
+  const blockProps = useBlockProps.save({ className });
+  const innerBlockProps = useInnerBlocksProps.save({ className: 'ghostkit-changelog-more' });
+
+  return (
+    <div {...blockProps}>
+      {!RichText.isEmpty(version) ? (
+        <RichText.Content tagName="span" className="ghostkit-changelog-version" value={version} />
+      ) : (
+        ''
+      )}
+      {!RichText.isEmpty(date) ? (
+        <RichText.Content tagName="h2" className="ghostkit-changelog-date" value={date} />
+      ) : (
+        ''
+      )}
+      <div {...innerBlockProps} />
+    </div>
+  );
 }
-
-export default BlockSave;
