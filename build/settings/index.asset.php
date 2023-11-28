1d5008380b535bab72b0938ddeef1235bc14795e
Fellan-91 <alexey.fellan@gmail.com>
2023-11-28 12:16:14 +0300

    build

--- build/settings/index.asset.php
+++ build/settings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'fefba9c67a2c20946dba');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5ce8eeb1cd3c89a1bde1');
