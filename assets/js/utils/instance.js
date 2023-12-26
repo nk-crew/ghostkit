f5b74e9a64f6dfd0d9167d68d5328330718ac514
Nikita <nkdevinfo@gmail.com>
2023-12-26 11:29:18 +0300

    fixed typo

--- assets/js/utils/instance.js
+++ assets/js/utils/instance.js
@@ -3,7 +3,7 @@
  * These instances contains many helpful data such as block APIs and extensions data.
  *
  * Example:
- * GHOSTKIT.instances.get(element, 'effects');
+ * GHOSTKIT.instance.get(element, 'effects');
  */
 const elementsMap = new Map();
 
@@ -12,42 +12,42 @@ export default {
 		return elementsMap;
 	},
 
-	get( element, key ) {
-		if ( elementsMap.has( element ) ) {
-			if ( key ) {
-				return elementsMap.get( element ).get( key ) || null;
+	get(element, key) {
+		if (elementsMap.has(element)) {
+			if (key) {
+				return elementsMap.get(element).get(key) || null;
 			}
 
-			return elementsMap.get( element ) || null;
+			return elementsMap.get(element) || null;
 		}
 
 		return null;
 	},
 
-	set( element, key, val ) {
-		if ( ! elementsMap.has( element ) ) {
-			elementsMap.set( element, new Map() );
+	set(element, key, val) {
+		if (!elementsMap.has(element)) {
+			elementsMap.set(element, new Map());
 		}
 
-		const instanceMap = elementsMap.get( element );
+		const instanceMap = elementsMap.get(element);
 
-		instanceMap.set( key, val );
+		instanceMap.set(key, val);
 	},
 
-	remove( element, key ) {
-		if ( ! elementsMap.has( element ) ) {
+	remove(element, key) {
+		if (!elementsMap.has(element)) {
 			return;
 		}
 
-		const instanceMap = elementsMap.get( element );
+		const instanceMap = elementsMap.get(element);
 
-		if ( key ) {
-			instanceMap.delete( key );
+		if (key) {
+			instanceMap.delete(key);
 		}
 
 		// Free up element references if there are no instances left for an element or key is missing.
-		if ( ! key || instanceMap.size === 0 ) {
-			elementsMap.delete( element );
+		if (!key || instanceMap.size === 0) {
+			elementsMap.delete(element);
 		}
 	},
 };
