86117611a35847feecd664debd1fa6e64d3b3838
Nikita <nkdevinfo@gmail.com>
2024-02-09 00:07:09 +0300

    added test to check the extension and deprecated core/list block migration

---
+++ tests/e2e/specs/extensions.spec.js
@@ -0,0 +1,29 @@
+import { expect, test } from '@wordpress/e2e-test-utils-playwright';
+
+test.describe('extensions', () => {
+	test.beforeAll(async ({ requestUtils }) => {
+		await requestUtils.activatePlugin('ghost-kit');
+	});
+
+	test('deprecated blocks should migrate and keep Ghost Kit extensions', async ({
+		page,
+		admin,
+	}) => {
+		await admin.createNewPost();
+
+		const padding = await page.evaluate(() => {
+			const { rawHandler } = wp.blocks;
+			const parsedBlocks = rawHandler({
+				HTML: `<!-- wp:list {"className":"ghostkit-custom-2b5h3i","ghostkit":{"styles":{"padding-left":"60"},"id":"2b5h3i"}} -->
+<ul class="ghostkit-custom-2b5h3i"><li>test</li></ul>
+<!-- /wp:list -->`,
+			});
+
+			return parsedBlocks[0]?.attributes?.ghostkit?.styles?.[
+				'padding-left'
+			];
+		});
+
+		await expect(padding).toBe('60');
+	});
+});
