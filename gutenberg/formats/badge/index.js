5878294d3f399128519d899cbd18fb97f0d5695a
Nikita <nkdevinfo@gmail.com>
2024-02-09 15:23:34 +0300

    added support for text color in Badge format
    removed outline badge format styles

--- gutenberg/formats/badge/index.js
+++ gutenberg/formats/badge/index.js
@@ -1,5 +1,6 @@
 import { RichTextToolbarButton } from '@wordpress/block-editor';
-import { Component } from '@wordpress/element';
+import { TabPanel } from '@wordpress/components';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import {
 	applyFormat,
@@ -7,12 +8,158 @@ import {
 	toggleFormat,
 } from '@wordpress/rich-text';
 
-import ApplyFilters from '../../components/apply-filters';
 import ColorPalette from '../../components/color-palette';
 import getIcon from '../../utils/get-icon';
 import BadgePopover from './badge-popover';
 
 export const name = 'ghostkit/badge';
+
+function parseCSS(css = '') {
+	return css.split(';').reduce((accumulator, rule) => {
+		if (rule) {
+			let [property, value] = rule.split(':');
+
+			if (property) {
+				property = property.trim();
+			}
+			if (value) {
+				value = value.trim();
+			}
+
+			if (property === 'color') {
+				accumulator.color = value;
+			}
+
+			if (property === 'background') {
+				accumulator.background = value;
+			}
+		}
+
+		return accumulator;
+	}, {});
+}
+
+function BadgeFormat(props) {
+	const { isActive, value, contentRef, onChange } = props;
+
+	const [openedPopover, setOpenedPopover] = useState(false);
+
+	// Close popover.
+	useEffect(() => {
+		if (!isActive && openedPopover) {
+			setOpenedPopover(false);
+		}
+	}, [isActive, openedPopover]);
+
+	function getCurrentColor() {
+		const { attributes } = getActiveFormat(value, name);
+
+		if (attributes && attributes.style) {
+			return parseCSS(attributes.style);
+		}
+
+		return {};
+	}
+
+	function toggleBadge(newBackground, newColor) {
+		const attributes = {};
+
+		if (newBackground) {
+			attributes.style = `background: ${newBackground};`;
+		}
+		if (newColor) {
+			if (attributes.style) {
+				attributes.style += ' ';
+			} else {
+				attributes.style = '';
+			}
+
+			attributes.style += `color: ${newColor};`;
+		}
+
+		const runFormat = newBackground ? applyFormat : toggleFormat;
+
+		onChange(
+			runFormat(value, {
+				type: name,
+				attributes,
+			})
+		);
+	}
+
+	let currentColor = {};
+
+	if (isActive) {
+		currentColor = getCurrentColor();
+	}
+
+	return (
+		<>
+			<RichTextToolbarButton
+				icon={getIcon('icon-badge')}
+				title={__('Badge', 'ghostkit')}
+				onClick={() => {
+					if (!isActive) {
+						toggleBadge();
+					}
+
+					setOpenedPopover(!openedPopover);
+				}}
+				isActive={isActive}
+			/>
+			{isActive && openedPopover ? (
+				<BadgePopover value={value} name={name} contentRef={contentRef}>
+					<TabPanel
+						tabs={[
+							{
+								name: 'background',
+								title: 'Background',
+							},
+							{
+								name: 'text',
+								title: 'Text',
+							},
+						]}
+						initialTabName="background"
+					>
+						{(tab) => {
+							if ('text' === tab.name) {
+								return (
+									<ColorPalette
+										label={__('Text', 'ghostkit')}
+										value={currentColor?.color || ''}
+										onChange={(val) => {
+											toggleBadge(
+												currentColor?.background || '',
+												val
+											);
+										}}
+										alpha
+									/>
+								);
+							}
+
+							return (
+								<ColorPalette
+									label={__('Background', 'ghostkit')}
+									value={currentColor?.background || ''}
+									onChange={(val) => {
+										toggleBadge(
+											val,
+											currentColor?.color || ''
+										);
+									}}
+									alpha
+									gradient
+								/>
+							);
+						}}
+					</TabPanel>
+				</BadgePopover>
+			) : null}
+		</>
+	);
+}
 
 export const settings = {
 	title: __('Badge', 'ghostkit'),
@@ -21,122 +168,5 @@ export const settings = {
 	attributes: {
 		style: 'style',
 	},
-	edit: class BadgeFormat extends Component {
-		constructor(props) {
-			super(props);
-
-			this.state = {
-				openedPopover: false,
-			};
-
-			this.getCurrentColor = this.getCurrentColor.bind(this);
-			this.toggleFormat = this.toggleFormat.bind(this);
-		}
-
-		componentDidUpdate() {
-			const { isActive } = this.props;
-
-			const { openedPopover } = this.state;
-
-			// Close popover.
-			if (!isActive && openedPopover) {
-				// eslint-disable-next-line react/no-did-update-set-state
-				this.setState({
-					openedPopover: false,
-				});
-			}
-		}
-
-		getCurrentColor() {
-			const { value } = this.props;
-
-			const { attributes } = getActiveFormat(value, name);
-
-			let color = '';
-
-			if (attributes && attributes.style) {
-				color = attributes.style
-					.replace(/^background:\s*/, '')
-					.replace(/;$/, '');
-			}
-
-			return color;
-		}
-
-		toggleFormat(color, toggle = true) {
-			const { value, onChange } = this.props;
-
-			const attributes = {};
-
-			if (color) {
-				attributes.style = `background: ${color};`;
-			} else {
-				this.setState({ openedPopover: true });
-			}
-
-			const runFormat = toggle ? toggleFormat : applyFormat;
-
-			onChange(
-				runFormat(value, {
-					type: name,
-					attributes,
-				})
-			);
-		}
-
-		render() {
-			const { value, isActive, contentRef } = this.props;
-
-			const { openedPopover } = this.state;
-
-			let currentColor = '';
-
-			if (isActive) {
-				currentColor = this.getCurrentColor();
-			}
-
-			return (
-				<>
-					<RichTextToolbarButton
-						icon={getIcon('icon-badge')}
-						title={__('Badge', 'ghostkit')}
-						onClick={() => {
-							if (!isActive) {
-								this.toggleFormat();
-							}
-
-							this.setState({
-								openedPopover: !openedPopover,
-							});
-						}}
-						isActive={isActive}
-					/>
-					{isActive && openedPopover ? (
-						<BadgePopover
-							value={value}
-							name={name}
-							contentRef={contentRef}
-						>
-							<ApplyFilters
-								name="ghostkit.editor-format.controls"
-								property="background"
-								value={currentColor}
-								format={name}
-								data={this}
-							>
-								<ColorPalette
-									value={currentColor}
-									onChange={(color) => {
-										this.toggleFormat(color, !color);
-									}}
-									alpha
-									gradient
-								/>
-							</ApplyFilters>
-						</BadgePopover>
-					) : null}
-				</>
-			);
-		}
-	},
+	edit: BadgeFormat,
 };
